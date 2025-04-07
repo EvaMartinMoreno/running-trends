1bde60091c68700e73a0c3aa6464b519cc516d5f
Eva <evamartinmoreno9@gmail.com>
2025-04-08 00:25:58 +0200

    README body - ext and clean file - Body project created

--- src/cleaning.py
+++ src/cleaning.py
@@ -124,11 +124,38 @@ complete_df["CCAA"] = complete_df["CCAA"].replace({
 display(complete_df.sample(15))
 
 #Save the DataFrame to a CSV file
-complete_df.to_csv(r"C:\Users\evaru\Downloads\EVOLVE\python\running-trends\data\PIB_CCAA.csv", index=False, sep=";")
+complete_df.to_csv(r"C:\Users\evaru\Downloads\EVOLVE\python\running-trends\data\running-trends-dataset.csv", index=False, sep=";")
 
+# Clean the data from "Renta ESP file"
+#Load the data
+import pandas as pd
+renta_df = pd.read_csv(r"C:\Users\evaru\Downloads\EVOLVE\python\running-trends\data\raw\RentaESP-ccaa.csv", sep=";")
+
+# Clean the data
+renta_cleaned_df = renta_df.pivot_table(index=renta_df.columns[0], columns=renta_df.columns[1], values=renta_df.columns[3])
+
+#Join the dataframe to our dataset 
+'''(como no puedo visualizarlo, no sé si este código está ok) - OJO con el orden de los valores'''
+# complete_df = renta_cleaned_df.merge(renta_cleaned_df, how='left', left_on='PIB_Capita', right_index=True)
+# print(complete_df.head())
+
+# Clean the data from "Tasa paro ESP file"
+# Load the data
+unemployment_df = pd.read_csv(r"C:\Users\evaru\Downloads\EVOLVE\python\running-trends\data\raw\TasaParoESP-ccaa.csv", sep=";")
+# Clean the data
+column_ordered = ['Periodo', 'Comunidades y Ciudades Autónomas', 'Total', 'Edad', 'Sexo']
+unemployment_df = unemployment_df[column_ordered]
+unemployment_df['Periodo'] = unemployment_df['Periodo'].astype(str).str[:4]
+unemployment_df = unemployment_df[unemployment_df['Sexo'] == 'Ambos sexos']
+unemployment_df = unemployment_df[unemployment_df['Edad'] == 'Total']
+unemployment_df['Total'] = unemployment_df['Total'].str.replace(',', '.').astype(float)
+sum_by_period = unemployment_df.groupby(['Periodo', 'Comunidades y Ciudades Autónomas'])['Total'].sum().reset_index()
+#Show the result
+print(sum_by_period.head())
+
+''' Ahora según el resultado que saquemos, tendremos que unirlo a nuestro dataset'''
 
 # Extract the data from Google trends
 # Extract the data from Runedia
-
-
+# Clean the data from "Licencias ESP file"
 
